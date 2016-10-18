7c485ff3c124daa1d2b414ac3a00c8a3f1589a98
FranciscoGJ <francisco.gonzalezj@mail.udp.cl>
2016-10-17 21:33:02 -0300

    ayenv 1.0

--- py/m_ayudantias/aylev.py
+++ py/m_ayudantias/aylev.py
@@ -2,21 +2,25 @@ from os import getpid
 from enviar_demonio import Cola_mensajes
 
 def proceso(aci,tx_in,tx_out,tx_sa):
-
-	data = {}
-	data["modo"] = "ay_levantas_solicitud"
-	data["cod_curso"]=tx_in[0:][:6]
-	data["cod_seccion"]=tx_in[7:][:8]
-	data["cod_ano"]=tx_in[9:][:12]
-	data["cod_sem"]=tx_in[13]
+    print tx_in
+    data = {}
+    data["modo"] = "ay_levantas_solicitud"
+    data["cod_curso"]=tx_in[0:][:7]
+    data["cod_seccion"]=tx_in[7:][:2]
+    data["cod_ano"]=tx_in[9:][:4]
+    data["cod_sem"]=tx_in[13]
     pid = getpid()
 
     cola = Cola_mensajes()
     respuesta = cola.enviar(pid,data)
+    print "aca",respuesta
 
+    from time import sleep
+    sleep(10)
 
+    tx_out = "0201"
 
-	return {'tx_out':tx_out,'tx_sa':tx_sa,'aci':aci}
+    return {'tx_out':tx_out,'tx_sa':tx_sa,'aci':aci}
 
 
 
