7b7d6f3bb348fbb2dfb5b8d42910dd0d4537c4ac
FranciscoGJ <francisco.gonzalezj@mail.udp.cl>
2016-10-25 10:12:59 -0300

    Listoco

--- form/inp/m_ayudantias/compilar_formulario.sh
+++ form/inp/m_ayudantias/compilar_formulario.sh
@@ -1,16 +1,13 @@
+gmon ayacp0.inp
 gmon ayacp1.inp
 gmon aycon0.inp
 gmon aycon1.inp
-gmon aycon2.inp
 gmon ayconp.inp
-gmon ayelis.inp
 gmon ayelp0.inp
 gmon ayelp1.inp
 gmon ayels0.inp
 gmon ayels1.inp
 gmon ayenv.inp
-gmon ayest.inp
 gmon aylev.inp
-gmon aynot.inp
 gmon aysel.inp
-mv
+mv *.fmt ../../../build/.
